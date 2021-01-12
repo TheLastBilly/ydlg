d982c23e3c1e9ac941dc31d43a7f8402887e8853
TheLastBilly <jtmonegro@gmail.com>
2021-01-11 22:02:12 -0500

    fixed bad reverse proxy

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,5 +2,13 @@
 
 python3 initApp.py
 
+usermod -u $PUID nginx
+groupmod -g $PGID www-data
+
+su - nginx
+
+chown -R nginx:www-data /downloads
+chown -R nginx:www-data /logs
+
 nginx -g 'daemon off;' &
-gunicorn --workers 3 --access-logfile /logs/gunicorn.log --bind unix:/tmp/ydlg.sock -m 007 wsgi -u nginx
+gunicorn --workers 3 --access-logfile /logs/gunicorn.log --error-logfile /logs/app.log --bind unix:/tmp/ydlg.sock -m 007 wsgi -u nginx --capture-output
